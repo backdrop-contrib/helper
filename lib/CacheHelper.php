2be8e7ef7888b91331ed9e88c0f9f089e4adb4ce
Dave Reid <dave@davereid.net>
2013-05-01 12:27:19 -0500

    Added CacheHelper library.

---
+++ lib/CacheHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+class CacheHelper {
+
+  public static function get($cid, $bin = 'cache') {
+    if ($cache = cache_get($cid, $bin)) {
+      if (!static::isCacheUnexpired($cache)) {
+        return FALSE;
+      }
+    }
+    return $cache;
+  }
+
+  public static function getMultiple(array &$cids, $bin = 'cache') {
+    $cache = cache_get_multiple($cids, $bin);
+    return array_filter($cache, array(get_called_class(), 'isCacheUnexpired'));
+  }
+
+  /**
+   * Check if a cache record is expired or not.
+   *
+   * Callback for array_filter() within CacheHelper::get() and
+   * CacheHelper::getMultiple().
+   *
+   * @param object $cache
+   *   A cache object from cache_get().
+   *
+   * @return bool
+   *   TRUE if the cache record has not yet expired, or FALSE otherwise.
+   */
+  public static function isCacheUnexpired($cache) {
+    if ($cache->expire > 0 && $cache->expire < REQUEST_TIME) {
+      return FALSE;
+    }
+  }
+}
