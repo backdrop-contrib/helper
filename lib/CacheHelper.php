ca9ccf69999645aea743f69b24858247b9b30d08
Dave Reid <dave@davereid.net>
2014-07-29 02:37:27 -0500

    Deprecated CacheHelper::httpRequest(). Added support for inspecting request headers when setting cache expiration.

--- lib/CacheHelper.php
+++ lib/CacheHelper.php
@@ -63,42 +63,9 @@ class CacheHelper {
   }
 
   /**
-   * Perform a cached HTTP request.
-   *
-   * @param string $url
-   *   A string containing a fully qualified URI.
-   * @param array $options
-   *   (optional) An array of options in the same format as
-   *   drupal_http_request().
-   *
-   * @return mixed
-   *   A response object in the same format as drupal_http_request().
+   * Deprecated.
    */
   public static function httpRequest($url, array $options = array()) {
-    $cid = static::cachedRequestGetCid($url, $options);
-    $bin = isset($options['cache']['bin']) ? $options['cache']['bin'] : 'cache';
-
-    if ($cid && $cache = static::get($cid, $bin)) {
-      return $cache->data;
-    }
-    else {
-      $response = drupal_http_request($url, $options);
-      if ($cid) {
-        $expire = isset($options['cache']['expire']) ? $options['cache']['expire'] : CACHE_TEMPORARY;
-        cache_set($cid, $response, $bin, $expire);
-      }
-      return $response;
-    }
-  }
-
-  public static function cachedRequestGetCid($url, array $options) {
-    if (isset($options['cache']) && $options['cache'] === FALSE) {
-      return FALSE;
-    }
-    if (isset($options['cache']['cid'])) {
-      return $options['cache']['cid'];
-    }
-    $cid_parts = array($url, serialize(array_diff_key($options, drupal_map_assoc(array('cache')))));
-    return 'http-request:' . drupal_hash_base64(serialize($cid_parts));
+    return HttpHelper::cachedRequest($url, $options);
   }
 }
--- lib/HttpHelper.php
+++ lib/HttpHelper.php
@@ -15,8 +15,10 @@ class HttpHelper {
         $cid = FALSE;
       }
       if ($cid) {
-        $expire = isset($options['cache']['expire']) ? $options['cache']['expire'] : CACHE_TEMPORARY;
-        cache_set($cid, $response, $bin, $expire);
+        $expire = static::cachedRequestGetExpire($response, $options);
+        if ($expire !== FALSE) {
+          cache_set($cid, $response, $bin, $expire);
+        }
       }
       return $response;
     }
@@ -29,7 +31,23 @@ class HttpHelper {
     if (isset($options['cache']['cid'])) {
       return $options['cache']['cid'];
     }
-    $cid_parts = array($url, serialize(array_diff_key($options, drupal_map_assoc(array('cache')))));
+    $cid_parts = array($url, serialize(array_diff_key($options, array('cache' => ''))));
     return 'http-request:' . drupal_hash_base64(serialize($cid_parts));
+  }
+
+  public static function cachedRequestGetExpire($response, $options) {
+    if (isset($options['cache']['expire'])) {
+      return $options['cache']['expire'];
+    }
+    elseif (!empty($response->headers['cache-control']) && strpos($response->headers['cache-control'], 'no-cache') !== FALSE) {
+      // Respect the Cache-Control: no-cache header.
+      return FALSE;
+    }
+    elseif (!empty($response->headers['expires']) && $expire = strtotime($response->headers['expires'])) {
+      return $expire;
+    }
+    else {
+      return CACHE_TEMPORARY;
+    }
   }
 }
